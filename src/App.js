689c4d81e1f3817fd78609a3e65ef5b399c48d82
Dhananjay <dhananjayman11@gmail.com>
2021-08-24 19:30:30 +0530

    alan ai setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import React, { useEffect } from 'react'
+import alanBtn from "@alan-ai/alan-sdk-web";
+
 import './App.css';
 
+const APIkey = '6917ed5aebd1b49dc91f310fad260be92e956eca572e1d8b807a3e2338fdd0dc/stage';
+
 const App = () => {
+
+  useEffect(() => {
+    alanBtn({
+      key: APIkey,
+      onCommand: ({ commandData }) => {
+        if (commandData === 'testCommand') {
+          alert('Welcome to News App');
+        }
+      }
+    });
+  }, []);
+
+
   return (
     <div>
       <h1>News App ALAN AI</h1>
